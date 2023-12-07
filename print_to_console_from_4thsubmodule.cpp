d35fa84ab1c8e2191bb623df50f2db92ec852c96
Lambourl <65184664+Lambourl@users.noreply.github.com>
2023-12-07 11:49:33 +0100

    Create print_to_console_from_4thsubmodule.cpp

---
+++ print_to_console_from_4thsubmodule.cpp
@@ -0,0 +1,7 @@
+#include <print_to_console_from_2ndsubmodule.hpp>
+
+#include <iostream>
+
+void a_4th_submodule::print_to_console() {
+  std::cout << "a_4th_submodule::print_to_console()" << std::endl;
+}
